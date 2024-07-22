e24319a26ef0a8d9239688255085206539e368a6
jaykumar kakadiya <jaykakdiya2411@gmail.com>
2024-07-22 19:42:00 -0400

    changed code

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 
 // 1. code with error for jslint
-function sayHello(name) {
-    if(name)
-    console.log("Hello, " + name);
-}
+// function sayHello(name) {
+//     if(name)
+//     console.log("Hello, " + name);
+// }
 
-sayHello("World")
+// sayHello("World")
 
 
 ///////////////////////////////////////////////////////////////////////////////////
 
 //2. code with no error for jslint
-// 'use strict'; // Enforces strict mode
-// function sayHello(name) {
-//     if (name) {
-//         console.log("Hello, " + name);
-//     }
-// }
+'use strict'; // Enforces strict mode
+function sayHello(name) {
+    if (name) {
+        console.log("Hello, " + name);
+    }
+}
 
-// sayHello("World");
+sayHello("World");
