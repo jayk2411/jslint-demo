7ceaab85587e095077f8389f1a50b53c4193db49
jaykumar kakadiya <jaykakdiya2411@gmail.com>
2024-07-22 19:25:38 -0400

    add lint workflow

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+
+// 1. code with error for jslint
 function sayHello(name) {
     if(name)
     console.log("Hello, " + name);
 }
 
 sayHello("World")
+
+
+///////////////////////////////////////////////////////////////////////////////////
+
+//2. code with no error for jslint
+// 'use strict'; // Enforces strict mode
+// function sayHello(name) {
+//     if (name) {
+//         console.log("Hello, " + name);
+//     }
+// }
+
+// sayHello("World");
